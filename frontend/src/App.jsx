9761c043619759cd29a043ad53bd9091503c5519
AkibAli <88201304+akibali1@users.noreply.github.com>
2025-11-05 11:07:27 +0530

    added home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    helo
-  )
-}
+    <div className="bg-black min-h-screen">
+      <Navbar />
+      <Hero />
+      
+    </div>
+  );
+};
 
-export default App
+export default App;
